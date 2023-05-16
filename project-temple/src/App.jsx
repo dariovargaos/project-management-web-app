66aee0f807fb150e45bcf288e649fd7920c55bd1
dariovargaos <dario.varga2012@gmail.com>
2023-05-16 12:17:53 +0200

    Add chakraUI styles to the app

--- project-temple/src/App.jsx
+++ project-temple/src/App.jsx
@@ -2,7 +2,15 @@ import { Box } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Box>test</Box>;
+  return (
+    <Box display="flex">
+      <Box flexGrow="1" p="0 60px">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dicta
+        sequi saepe provident temporibus impedit obcaecati quidem, beatae at
+        veritatis, quod nisi ut quisquam aspernatur, autem minus hic? Amet totam
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
