ed773d799a5ad194b925debda6a39647d41093eb
dariovargaos <dario.varga2012@gmail.com>
2023-05-18 20:12:17 +0200

    Add thumbnail upload in signUp form

--- project-temple/src/pages/signup/Signup.jsx
+++ project-temple/src/pages/signup/Signup.jsx
@@ -4,6 +4,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
@@ -14,10 +15,37 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailErorr, setThumbnailError] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, displayName);
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be and image");
+      return;
+    }
+
+    if (selected.size > 100000) {
+      setThumbnailError("Image file size must be less than 100kb.");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+
+    console.log("thumbnail updated");
   };
 
   return (
@@ -29,7 +57,7 @@ export default function Signup() {
         p="40px"
         boxShadow="3px 3px 5px rgba(0,0,0, 0.05)"
       >
-        <form className="auth-form" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <Heading as="h2" fontSize="1.5em" mb="8px">
             Sign up
           </Heading>
@@ -60,9 +88,10 @@ export default function Signup() {
               value={displayName}
             />
           </FormControl>
-          <FormControl isRequired mb="10px">
+          <FormControl isRequired isInvalid={thumbnailErorr} mb="10px">
             <FormLabel>profile thumbnail:</FormLabel>
-            <Input type="file" required />
+            <Input type="file" required onChange={handleFileChange} />
+            <FormErrorMessage>{thumbnailErorr}</FormErrorMessage>
           </FormControl>
           <Button colorScheme="whatsapp" type="submit">
             Sign up
