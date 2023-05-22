4b0cd692f6e8f252df480ab303d9af6e1a24fa09
dariovargaos <dario.varga2012@gmail.com>
2023-05-22 12:34:58 +0200

    Add route guarding

--- project-temple/src/App.jsx
+++ project-temple/src/App.jsx
@@ -3,7 +3,10 @@ import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
 } from "react-router-dom";
+
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages and components
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -13,20 +16,29 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import RootLayout from "./layouts/RootLayout";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Dashboard />} />
-      <Route path="create" element={<Create />} />
-      <Route path="projects/:id" element={<Project />} />
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<Signup />} />
-    </Route>
-  )
-);
-
 function App() {
-  return <RouterProvider router={router} />;
+  const { user, authIsReady } = useAuthContext();
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={user ? <Dashboard /> : <Navigate to="login" />} />
+        <Route
+          path="create"
+          element={user ? <Create /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="projects/:id"
+          element={user ? <Project /> : <Navigate to="/login" />}
+        />
+        <Route path="login" element={!user ? <Login /> : <Navigate to="/" />} />
+        <Route
+          path="signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+      </Route>
+    )
+  );
+  return authIsReady && <RouterProvider router={router} />;
 }
 
 export default App;
