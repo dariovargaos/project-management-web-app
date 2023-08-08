14da8d472fea8283c55ccc9c092efdd237c11dbd
dariovargaos <dario.varga2012@gmail.com>
2023-08-08 18:43:49 +0200

    add minor fixes to the home page

--- project-temple/src/layouts/Home.jsx
+++ project-temple/src/layouts/Home.jsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Text, VStack, Image } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Heading, Text, VStack, Image, Link } from "@chakra-ui/react";
 
 import Navbar from "../components/Navbar";
 
@@ -13,7 +14,17 @@ export default function Home() {
         <VStack>
           <Heading>Welcome to the Temple</Heading>
           <Image src={Temple} alt="temple" h="200px" />
-          <Text>Please login or if you don't have account sign up.</Text>
+          <Text>
+            Please{" "}
+            <Link as={RouterLink} to="/login" fontWeight="bold">
+              login
+            </Link>{" "}
+            or if you don't have account{" "}
+            <Link as={RouterLink} to="/signup" fontWeight="bold">
+              sign up
+            </Link>
+            .
+          </Text>
         </VStack>
       </Box>
     </Box>
